c0a8e12c3b71ad493d4b5d059cd079cfb943a863
Paulo Abreu <paullooabreu@gmail.com>
2019-09-06 15:11:21 -0300

    utilizando localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  //carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  };
+
+  //Salvar os dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories))
+    }
   };
 
   handleInputChange = e => {
@@ -17,36 +37,57 @@ export default class Main extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
+    this.setState({ loading: true });
 
-    const { newRepo } = this.state;
+    const { newRepo, repositories } = this.state;
 
     const response = await api.get(`/repos/${newRepo}`);
 
-    console.log(response.data);
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
   };
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, repositories, loading } = this.state;
 
     return (
       <Container>
         <h1>
           <FaGithubAlt />
-          Repositorios
+          Repositórios
      </h1>
 
         <Form onSubmit={this.handleSubmit}>
           <input
             type='text'
-            placeholder='Adicionar repositorio'
+            placeholder='Adicionar Repositório'
             value={newRepo}
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton>
-            <FaPlus color="#FFF" size={14} />
+          <SubmitButton loading={loading}>
+            {loading ? (<FaSpinner color="#FFF" size={14} />
+            ) : (
+                <FaPlus color="#FFF" size={14} />
+              )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <a href="#">Detalhes</a>
+            </li>
+          ))}
+        </List>
       </Container >
     );
   }
